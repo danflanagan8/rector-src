8863f831a17b37332498d417cde9771e9ccb4fed
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-17 18:24:53 +0700

    fix return function no scope part

--- src/NodeAnalyzer/UnreachableStmtAnalyzer.php
+++ src/NodeAnalyzer/UnreachableStmtAnalyzer.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Rector\Core\NodeAnalyzer;
 
 use PhpParser\Node;
-use PhpParser\Node\Stmt;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 
 final class UnreachableStmtAnalyzer
@@ -14,26 +13,23 @@ final class UnreachableStmtAnalyzer
      * in case of unreachable stmts, no other node will have available scope
      * recursively check previous expressions, until we find nothing or is_unreachable
      */
-    public function isStmtPHPStanUnreachable(?Stmt $stmt): bool
+    public function isStmtPHPStanUnreachable(?Node $node): bool
     {
-        if (! $stmt instanceof Stmt) {
+        if (! $node instanceof Node) {
             return false;
         }
 
-        if ($stmt->getAttribute(AttributeKey::IS_UNREACHABLE) === true) {
+        if ($node->getAttribute(AttributeKey::IS_UNREACHABLE) === true) {
             // here the scope is never available for next stmt so we have nothing to refresh
             return true;
         }
 
-        $previousStmt = $stmt->getAttribute(AttributeKey::PREVIOUS_NODE);
+        $previousStmt = $node->getAttribute(AttributeKey::PREVIOUS_NODE);
         if (! $previousStmt instanceof Node) {
-            return false;
+            $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
+            return $this->isStmtPHPStanUnreachable($parentNode);
         }
 
-        if ($previousStmt instanceof Stmt) {
-            return $this->isStmtPHPStanUnreachable($previousStmt);
-        }
-
-        return true;
+        return $this->isStmtPHPStanUnreachable($previousStmt);
     }
 }
